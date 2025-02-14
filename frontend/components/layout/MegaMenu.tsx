cbfdfbfd46f5ec48265c7566fd49ca6827a54bc4
kvin-stphns <kvin.stphns@gmail.com>
2025-02-13 22:24:07 -0500

    Update MegaMenu.tsx
    updated clicking out to close functionality

--- frontend/components/layout/MegaMenu.tsx
+++ frontend/components/layout/MegaMenu.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Fragment, useState } from 'react'
+import { Fragment, useState, useEffect } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 
 interface MegaMenuProps {
@@ -20,228 +20,234 @@ const MegaMenu = ({ category }: MegaMenuProps) => {
   const [showCategoriesMenu, setShowCategoriesMenu] = useState(false)
   const [showDesignersMenu, setShowDesignersMenu] = useState(false)
   const [showRankingsMenu, setShowRankingsMenu] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const handleLinkClick = (link: string) => {
+    if (link === 'Discover') setShowDiscoverMenu(!showDiscoverMenu)
+    if (link === 'Brands') setShowBrandsMenu(!showBrandsMenu)
+    if (link === 'Categories') setShowCategoriesMenu(!showCategoriesMenu)
+    if (link === 'Designers') setShowDesignersMenu(!showDesignersMenu)
+    if (link === 'Rankings') setShowRankingsMenu(!showRankingsMenu)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement
+      if (!target.closest('.menu-container')) {
+        setShowDiscoverMenu(false)
+        setShowBrandsMenu(false)
+        setShowCategoriesMenu(false)
+        setShowDesignersMenu(false)
+        setShowRankingsMenu(false)
+        setIsMenuOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => document.removeEventListener('mousedown', handleClickOutside)
+  }, [])
 
   return (
     <Menu as="div" className="relative">
-      <Menu.Button className="text-sm tracking-[0.25em] hover:text-gray-500 transition-colors">
-        {category}
-      </Menu.Button>
+      {({ open }) => {
+        if (open !== isMenuOpen) setIsMenuOpen(open)
+        return (
+          <>
+            <Menu.Button className="text-sm tracking-[0.25em] hover:text-gray-500 transition-colors">
+              {category}
+            </Menu.Button>
 
-      <Transition
-        as={Fragment}
-        enter="transition duration-100 ease-out"
-        enterFrom="transform translate-x-[-100%]"
-        enterTo="transform translate-x-0"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform translate-x-0"
-        leaveTo="transform translate-x-[-100%]"
-      >
-        <Menu.Items className="fixed left-0 top-[140px] bottom-0 w-[400px] bg-white z-[48] overflow-y-auto">
-          {/* White overlay to hide the border lines */}
-          <div className="absolute -top-[1px] left-0 right-0 h-[2px] bg-white z-[1004]" />
-          
-          <div className="relative h-full border-r border-black">
-            {/* Content wrapper */}
-            <div className="min-h-full flex flex-col justify-between">
-              {/* Top Links */}
-              <div className="py-12">
-                {topLinks.map((link) => (
-                  <Menu.Item key={link}>
-                    {({ active }) => (
+            <div className="menu-container">
+              <Transition
+                show={isMenuOpen}
+                as={Fragment}
+                enter="transition duration-100 ease-out"
+                enterFrom="transform translate-x-[-100%]"
+                enterTo="transform translate-x-0"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform translate-x-0"
+                leaveTo="transform translate-x-[-100%]"
+              >
+                <Menu.Items static className="fixed left-0 top-[140px] bottom-0 w-[400px] bg-white z-[48] overflow-y-auto">
+                  {/* White overlay to hide the border lines */}
+                  <div className="absolute -top-[1px] left-0 right-0 h-[2px] bg-white z-[1004]" />
+                  
+                  <div className="relative h-full border-r border-black">
+                    {/* Content wrapper */}
+                    <div className="min-h-full flex flex-col justify-between">
+                      {/* Top Links */}
+                      <div className="py-12">
+                        {topLinks.map((link) => (
+                          <Menu.Item key={link}>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
+                                onClick={(e) => {
+                                  e.preventDefault()
+                                  handleLinkClick(link)
+                                }}
+                              >
+                                {link}
+                              </a>
+                            )}
+                          </Menu.Item>
+                        ))}
+                      </div>
+
+                      {/* Bottom Links */}
+                      <div className="py-8">
+                        {bottomLinks.map((link) => (
+                          <Menu.Item key={link}>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className="block px-12 py-2 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
+                              >
+                                {link}
+                              </a>
+                            )}
+                          </Menu.Item>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </Menu.Items>
+              </Transition>
+
+              {/* Layer 2 - Discover Menu */}
+              <Transition
+                show={showDiscoverMenu}
+                as={Fragment}
+                enter="transition duration-100 ease-out"
+                enterFrom="transform translate-x-[-100%]"
+                enterTo="transform translate-x-0"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform translate-x-0"
+                leaveTo="transform translate-x-[-100%]"
+              >
+                <div className="fixed left-[400px] top-[140px] bottom-0 w-[400px] bg-white z-[47] border-r border-black overflow-y-auto">
+                  <div className="py-12">
+                    {discoverLinks.map((link) => (
                       <a
+                        key={link}
                         href="#"
                         className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
-                        onMouseEnter={() => {
-                          if (link === 'Discover') setShowDiscoverMenu(true)
-                          if (link === 'Brands') setShowBrandsMenu(true)
-                          if (link === 'Categories') setShowCategoriesMenu(true)
-                          if (link === 'Designers') setShowDesignersMenu(true)
-                          if (link === 'Rankings') setShowRankingsMenu(true)
-                        }}
-                        onMouseLeave={() => {
-                          if (link === 'Discover') setShowDiscoverMenu(false)
-                          if (link === 'Brands') setShowBrandsMenu(false)
-                          if (link === 'Categories') setShowCategoriesMenu(false)
-                          if (link === 'Designers') setShowDesignersMenu(false)
-                          if (link === 'Rankings') setShowRankingsMenu(false)
-                        }}
                       >
                         {link}
                       </a>
-                    )}
-                  </Menu.Item>
-                ))}
-              </div>
+                    ))}
+                  </div>
+                </div>
+              </Transition>
 
-              {/* Bottom Links */}
-              <div className="py-8">
-                {bottomLinks.map((link) => (
-                  <Menu.Item key={link}>
-                    {({ active }) => (
+              {/* Layer 2 - Brands Menu */}
+              <Transition
+                show={showBrandsMenu}
+                as={Fragment}
+                enter="transition duration-100 ease-out"
+                enterFrom="transform translate-x-[-100%]"
+                enterTo="transform translate-x-0"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform translate-x-0"
+                leaveTo="transform translate-x-[-100%]"
+              >
+                <div className="fixed left-[400px] top-[140px] bottom-0 w-[400px] bg-white z-[47] border-r border-black overflow-y-auto">
+                  <div className="py-12">
+                    {brandLinks.map((link) => (
                       <a
+                        key={link}
                         href="#"
-                        className="block px-12 py-2 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
+                        className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
                       >
                         {link}
                       </a>
-                    )}
-                  </Menu.Item>
-                ))}
-              </div>
+                    ))}
+                  </div>
+                </div>
+              </Transition>
+
+              {/* Layer 2 - Categories Menu */}
+              <Transition
+                show={showCategoriesMenu}
+                as={Fragment}
+                enter="transition duration-100 ease-out"
+                enterFrom="transform translate-x-[-100%]"
+                enterTo="transform translate-x-0"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform translate-x-0"
+                leaveTo="transform translate-x-[-100%]"
+              >
+                <div className="fixed left-[400px] top-[140px] bottom-0 w-[400px] bg-white z-[47] border-r border-black overflow-y-auto">
+                  <div className="py-12">
+                    {categoryLinks.map((link) => (
+                      <a
+                        key={link}
+                        href="#"
+                        className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
+                      >
+                        {link}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </Transition>
+
+              {/* Layer 2 - Designers Menu */}
+              <Transition
+                show={showDesignersMenu}
+                as={Fragment}
+                enter="transition duration-100 ease-out"
+                enterFrom="transform translate-x-[-100%]"
+                enterTo="transform translate-x-0"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform translate-x-0"
+                leaveTo="transform translate-x-[-100%]"
+              >
+                <div className="fixed left-[400px] top-[140px] bottom-0 w-[400px] bg-white z-[47] border-r border-black overflow-y-auto">
+                  <div className="py-12">
+                    {designerLinks.map((link) => (
+                      <a
+                        key={link}
+                        href="#"
+                        className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
+                      >
+                        {link}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </Transition>
+
+              {/* Layer 2 - Rankings Menu */}
+              <Transition
+                show={showRankingsMenu}
+                as={Fragment}
+                enter="transition duration-100 ease-out"
+                enterFrom="transform translate-x-[-100%]"
+                enterTo="transform translate-x-0"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform translate-x-0"
+                leaveTo="transform translate-x-[-100%]"
+              >
+                <div className="fixed left-[400px] top-[140px] bottom-0 w-[400px] bg-white z-[47] border-r border-black overflow-y-auto">
+                  <div className="py-12">
+                    {rankingLinks.map((link) => (
+                      <a
+                        key={link}
+                        href="#"
+                        className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
+                      >
+                        {link}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </Transition>
             </div>
-          </div>
-        </Menu.Items>
-      </Transition>
-
-      {/* Layer 2 - Discover Menu */}
-      <Transition
-        show={showDiscoverMenu}
-        as={Fragment}
-        enter="transition duration-100 ease-out"
-        enterFrom="transform translate-x-[-100%]"
-        enterTo="transform translate-x-0"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform translate-x-0"
-        leaveTo="transform translate-x-[-100%]"
-      >
-        <div 
-          className="fixed left-[400px] top-[140px] bottom-0 w-[400px] bg-white z-[47] border-r border-black overflow-y-auto"
-          onMouseEnter={() => setShowDiscoverMenu(true)}
-          onMouseLeave={() => setShowDiscoverMenu(false)}
-        >
-          <div className="py-12">
-            {discoverLinks.map((link) => (
-              <a
-                key={link}
-                href="#"
-                className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
-              >
-                {link}
-              </a>
-            ))}
-          </div>
-        </div>
-      </Transition>
-
-      {/* Layer 2 - Brands Menu */}
-      <Transition
-        show={showBrandsMenu}
-        as={Fragment}
-        enter="transition duration-100 ease-out"
-        enterFrom="transform translate-x-[-100%]"
-        enterTo="transform translate-x-0"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform translate-x-0"
-        leaveTo="transform translate-x-[-100%]"
-      >
-        <div 
-          className="fixed left-[400px] top-[140px] bottom-0 w-[400px] bg-white z-[47] border-r border-black overflow-y-auto"
-          onMouseEnter={() => setShowBrandsMenu(true)}
-          onMouseLeave={() => setShowBrandsMenu(false)}
-        >
-          <div className="py-12">
-            {brandLinks.map((link) => (
-              <a
-                key={link}
-                href="#"
-                className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
-              >
-                {link}
-              </a>
-            ))}
-          </div>
-        </div>
-      </Transition>
-
-      {/* Layer 2 - Categories Menu */}
-      <Transition
-        show={showCategoriesMenu}
-        as={Fragment}
-        enter="transition duration-100 ease-out"
-        enterFrom="transform translate-x-[-100%]"
-        enterTo="transform translate-x-0"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform translate-x-0"
-        leaveTo="transform translate-x-[-100%]"
-      >
-        <div 
-          className="fixed left-[400px] top-[140px] bottom-0 w-[400px] bg-white z-[47] border-r border-black overflow-y-auto"
-          onMouseEnter={() => setShowCategoriesMenu(true)}
-          onMouseLeave={() => setShowCategoriesMenu(false)}
-        >
-          <div className="py-12">
-            {categoryLinks.map((link) => (
-              <a
-                key={link}
-                href="#"
-                className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
-              >
-                {link}
-              </a>
-            ))}
-          </div>
-        </div>
-      </Transition>
-
-      {/* Layer 2 - Designers Menu */}
-      <Transition
-        show={showDesignersMenu}
-        as={Fragment}
-        enter="transition duration-100 ease-out"
-        enterFrom="transform translate-x-[-100%]"
-        enterTo="transform translate-x-0"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform translate-x-0"
-        leaveTo="transform translate-x-[-100%]"
-      >
-        <div 
-          className="fixed left-[400px] top-[140px] bottom-0 w-[400px] bg-white z-[47] border-r border-black overflow-y-auto"
-          onMouseEnter={() => setShowDesignersMenu(true)}
-          onMouseLeave={() => setShowDesignersMenu(false)}
-        >
-          <div className="py-12">
-            {designerLinks.map((link) => (
-              <a
-                key={link}
-                href="#"
-                className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
-              >
-                {link}
-              </a>
-            ))}
-          </div>
-        </div>
-      </Transition>
-
-      {/* Layer 2 - Rankings Menu */}
-      <Transition
-        show={showRankingsMenu}
-        as={Fragment}
-        enter="transition duration-100 ease-out"
-        enterFrom="transform translate-x-[-100%]"
-        enterTo="transform translate-x-0"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform translate-x-0"
-        leaveTo="transform translate-x-[-100%]"
-      >
-        <div 
-          className="fixed left-[400px] top-[140px] bottom-0 w-[400px] bg-white z-[47] border-r border-black overflow-y-auto"
-          onMouseEnter={() => setShowRankingsMenu(true)}
-          onMouseLeave={() => setShowRankingsMenu(false)}
-        >
-          <div className="py-12">
-            {rankingLinks.map((link) => (
-              <a
-                key={link}
-                href="#"
-                className="block px-12 py-4 text-sm tracking-[0.25em] hover:text-gray-500 transition-colors"
-              >
-                {link}
-              </a>
-            ))}
-          </div>
-        </div>
-      </Transition>
+          </>
+        )
+      }}
     </Menu>
   )
 }
