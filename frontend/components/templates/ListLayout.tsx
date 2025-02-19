42b12090c6a198f9dfc992e5b0b2c6cf6a98eaed
kvin-stphns <kvin.stphns@gmail.com>
2025-02-19 00:40:44 -0500

    Update ListLayout.tsx

--- frontend/components/templates/ListLayout.tsx
+++ frontend/components/templates/ListLayout.tsx
@@ -79,8 +79,8 @@ const ListLayout = ({ title, items, category, section, subsection }: ListLayoutP
         </div>
 
         {/* Preview Section */}
-        <div className="w-[65%] pt-[300px]">
-          <div className="h-[calc(100vh-300px)] relative">
+        <div className="w-[65%] pt-[260px]">
+          <div className="h-[calc(100vh-260px)] relative">
             <Image 
               src={activeItem.image}
               alt={activeItem.name}
